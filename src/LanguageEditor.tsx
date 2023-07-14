9f5a7d50e1edde379cb3a4582a54f335bc863bd6
ThisGameZR <timezazare@gmail.com>
2023-07-14 16:37:11 +0700

    React missingg??

--- src/LanguageEditor.tsx
+++ src/LanguageEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLanguageProvider } from "./LanguageProvider";
 import { Box, Autocomplete, TextField, Stack } from "@mui/material";
 import EditableTable from "./EditableTable";
